b4286ccbe1b4d30c8713c3aaabdf9b7ca1a6e19c
korea102411 <korea102411@gmail.com>
2021-04-25 20:36:37 -0700

    commit 04/25

---
+++ movezeros.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def moveZeroes(nums: List[int]) -> None:
+    j = 0;
+    n = len(nums);
+    for num in nums:
+        if (num != 0):
+            nums[j] = num
+            j += 1
+
+    for x in range(j, n):
+        nums[x] = 0
+
+
+class Solution:
+    pass
